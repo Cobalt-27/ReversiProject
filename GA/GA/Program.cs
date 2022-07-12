63cb9db0689659a5a942e229d06d784a9e299590
Cobalt-27 <2870206676@qq.com>
2022-07-12 14:10:43 +0800

    add deadpos and safe score

---
+++ GA/GA/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int[,] Cross(int[,] a, int[,] b)
+{
+    return a;
+}
+
+int[,] GenerateWeights()
+{
+    var res = new int[4,4];
+    return res;
+}
+
+async Task<int> NewJob()
+{
+    return await Task.Run(() =>
+        {
+            Console.WriteLine("Hello, World!");
+            var path = @"C:\Users\Clover\Desktop\AI\Reversi\reversi.py";
+            var eng = IronPython.Hosting.Python.CreateEngine();
+            var scope = eng.CreateScope();
+            return eng.Execute<int>(File.ReadAllText(path));
+        });
+}
+
--- reversi.py
+++ reversi.py
@@ -1,10 +1,7 @@
-import enum
-from secrets import choice
-import numpy as np
+# import numpy as np
 import random
 import time
 
-from pandas import reset_option
 
 COLOR_BLACK = -1
 COLOR_WHITE = 1
@@ -16,7 +13,7 @@ random.seed(0)
 class AI(object):
     # chessboard_size, color, time_out passed from agent
     def __init__(self, chessboard_size, color, time_out):
-        v=1
+        v = 1
         self.chessboard_size = chessboard_size
         # You are white or black
         self.color = color
@@ -27,47 +24,42 @@ class AI(object):
         self.candidate_list = []
 
         self.wcond = [
-            0, 20, 45, 54
+            0, 20, 45, 55
         ]
-        # count, move, pos
+        # count, move, pos, deadpos,safe
         self.weights = [
-            (2, 2, 1),  # 0+
-            (3, 2, 1),  # 20+
-            (10, 1, 2),  # 45+
-            (20, 0, 0),  # 55+
+            (0, 1, 2, 1, 1),  # 0+
+            (0, 2, 2, 1, 1),  # 20+
+            (1, 2, 2, 1, 1),  # 45+
+            (1, 0, 0, 0, 0),  # 55+
         ]
         self.all = []
         for x in range(self.chessboard_size):
             for y in range(self.chessboard_size):
                 self.all.append((x, y))
-        self.maskcore = [
-            [-100, -10, -20, -20],
-            [-10, 20, 20, 20],
-            [-20, 20, 1, 1, ],
-            [-20, 20, 1, 1],
+        self.pos_mask_core = [
+            [-30, 1, -2, -2],
+            [1, 0, 0, 0],
+            [-2, 0, 0, 0, ],
+            [-2, 0, 0, 0],
         ]
-        self.genmat()
-        # show(self.mask)
-        # print(self.mask)
+        self.move_mask_core = [
+            [0, 5, 0, 0],
+            [5, 4, 4, 4],
+            [0, 4, 1, 1, ],
+            [0, 4, 1, 1],
+        ]
+        self.posmask = self.genmat(self.pos_mask_core)
+        self.movemask = self.genmat(self.move_mask_core)
 
-    def setseed(self, seed):
-        m0 = seed[0]
-        m1 = seed[1]
-        m2 = seed[2]
-        m3 = seed[3]
-        m4 = seed[4]
-        self.maskcore = [
-            [m0, m1, m2, m2],
-            [m1, m3, m3, m3],
-            [m2, m3, m4, m4, ],
-            [m2, m3, m4, m4],
-        ]
-        self.mask=self.genmat(self.maskcore)
+    def setweights(self, w, c):
+        self.weights = w
+        self.wcond = c
 
     def next(self, board, pos, color):
         res = [list(row) for row in board]
         cnt = 0
-        res[pos[0]][pos[1]]=color
+        res[pos[0]][pos[1]] = color
         for dirx in [-1, 0, 1]:
             for diry in [-1, 0, 1]:
                 if (dirx != 0 or diry != 0):
@@ -80,15 +72,8 @@ class AI(object):
                                 if step > 1:
                                     for t in range(1, step):
                                         if res[pos[0]+dirx * t][pos[1]+diry*t] == color or res[pos[0]+dirx * t][pos[1]+diry*t] == COLOR_NONE:
-                                            # show(res)
-                                            # show(board)
-                                            # exit(f'flip error {pos} {(pos[0]+dirx * t,pos[1]+diry*t)} {color}')
-                                            # print()
                                             raise Exception(
                                                 f'flip error {pos} {(pos[0]+dirx * t,pos[1]+diry*t)}')
-                                        # else:
-                                        #     print('ok')
-                                        #     show(res)
                                         res[pos[0]+dirx *
                                             t][pos[1]+diry*t] = color
                                         cnt += 1
@@ -99,7 +84,7 @@ class AI(object):
             raise Exception(f'no flip in {pos}')
         return res
 
-    def genmat(self,core):
+    def genmat(self, core):
         res = [[] for _ in range(8)]
         for x, y in self.all:
             if x < 4:
@@ -110,7 +95,7 @@ class AI(object):
                 cy = y
             else:
                 cy = 7-y
-            res.append(core[cx][cy])
+            res[x].append(core[cx][cy])
         return res
 
     def timeout(self):
@@ -123,7 +108,7 @@ class AI(object):
                 return self.weights[-i]
 
     def inside(self, pos):
-        return 0 <= pos[0] < self.chessboard_size and 0 <= pos[1] < self.chessboard_size
+        return 0 <= pos[0] < 8 and 0 <= pos[1] < 8
 
     def flipped(self, chessboard, pos, color):
         res = 0
@@ -146,17 +131,21 @@ class AI(object):
     def put(self, board, pos, color):
         board[pos[0]][pos[1]] = color
 
-    def get(self, chessboard, pos):
-        return chessboard[pos[0]][pos[1]]
+    def get(self, board, pos):
+        return board[pos[0]][pos[1]]
 
     def minimax(self, board, term, alpha, beta, stop):
-        me=self.color*term
-        cp=self.canput(board, me)
+        me = self.color*term
+        cp = self.canput(board, me)
         if self.timeout():
             return 0
-        if stop == 0 or self.timeout() or len(cp)==0:
-            return self.eval(board)
+        if stop == 0 or self.timeout() or len(cp) == 0:
+            return self.util(board, me)
         # res = -1e5 if term == 1 else 1e5
+        scores = {}
+        for pos in cp:
+            scores[pos] = self.util(self.next(board, pos, me), me)
+        cp.sort(key=lambda x: scores[x])
         for pos in cp:
             nextboard = self.next(board, pos, me)
             x = self.minimax(nextboard, -term, alpha, beta, stop-1)
@@ -169,8 +158,8 @@ class AI(object):
                 beta = min(beta, x)
                 # res = min(res, x)
             if alpha >= beta:
-                return alpha if term==1 else beta
-        return alpha if term==1 else beta
+                return alpha if term == 1 else beta
+        return alpha if term == 1 else beta
 
     def steps(self, board):
         cnt = 0
@@ -179,36 +168,66 @@ class AI(object):
                 cnt += 1
         return cnt
 
-    def count_score(self, board):  # ~10
+    def deadpos_score(self, board, color):
+        cnt = 0
+        for x in range(8):
+            val = color*sum(board[x])
+            if val > 6:
+                cnt -= 10
+            if val < 6:
+                cnt += 10
+        for y in range(8):
+            val = color*sum(board[x][y] for x in range(8))
+            if val > 6:
+                cnt -= 10
+            if val < 6:
+                cnt += 10
+        return cnt
+
+    def safe_score(self, board, color):
+        rival = -color
+        safe = set(x for x in self.all if self.get(board,x) == color)
+        for pos in self.canput(board, rival):
+            afterput = self.next(board, pos, rival)
+            safe = set(x for x in safe if self.get(afterput, x) == color)
+        return len(safe)
+
+    def count_score(self, board, color):  # ~10
         res = 0
         for pos in self.all:
-            if self.get(board, pos) == self.color:
+            if self.get(board, pos) == color:
                 res -= 1
-            elif self.get(board, pos) == -self.color:
+            elif self.get(board, pos) == -color:
                 res += 1
         return res
 
-    def move_score(self, board):  # ~10
+    def move_score(self, board, color):  # ~10
         res = 0
-        for pos in self.canput(board,self.color):
-            if self.get(board, pos) == self.color:
-                res += self.mask[pos[0]][pos[1]]
-            elif self.get(board, pos) == -self.color:
-                res -= self.mask[pos[0]][pos[1]]
+        for pos in self.canput(board, color):
+            # if self.get(board, pos) == color:
+            res += self.get(self.movemask, pos)
+            # elif self.get(board, pos) == -color:
+            #     res -= self.get(self.movemask, pos)
         return res
 
-    def pos_score(self, board):  # ~10
+    def pos_score(self, board, color):  # ~10
         res = 0
         for pos in self.all:
-            if self.get(board, pos) == self.color:
-                res += self.mask[pos[0]][pos[1]]
-            elif self.get(board, pos) == -self.color:
-                res -= self.mask[pos[0]][pos[1]]
+            if self.get(board, pos) == color:
+                res += self.get(self.posmask, pos)
+            elif self.get(board, pos) == -color:
+                res -= self.get(self.posmask, pos)
         return res
 
-    def eval(self, board):
+    def util(self, board, color):
         w = self.getweight(board)
-        return self.count_score(board)*w[0]+self.move_score(board)*w[1]+self.pos_score(board)*w[2]
+        if color == self.color:
+            sign = 1
+        else:
+            sign = -1
+        val = self.count_score(
+            board, color)*w[0]+self.move_score(board, color)*w[1]+self.pos_score(board, color)*w[2]+self.deadpos_score(board, color)*w[3]+self.safe_score(board, color)*w[4]
+        return val*sign
 
     def canput(self, board, color):
         res = []
@@ -226,25 +245,32 @@ class AI(object):
         choices = self.canput(chessboard, self.color)
         score = {}
         for stop in range(2, 20):
+            temp = {}
+            finish = True
             if self.timeout():
                 break
             for x in choices:
-                s = self.minimax(self.next(chessboard,x,self.color), -1, -1e6, 1e6, stop)
+                s = self.minimax(
+                    self.next(chessboard, x, self.color), -1, -1e6, 1e6, stop)
                 if self.timeout():
+                    finish = False
                     break
-                score[x]=s
+                temp[x] = s
+            if finish:
+                score = temp
         choices.sort(key=lambda x: score[x])
         print(choices)
         print(score)
         self.candidate_list = choices
         # print(self.candidate_list)
 
+
 def show(board):
     # for y in range(8):
     #     print(y,end='')
     # print('')
-    for i,row in enumerate(board):
-        print(i,end='')
+    for i, row in enumerate(board):
+        print(i, end='')
         for item in row:
             if item == COLOR_NONE:
                 print('-', end='')
@@ -253,61 +279,55 @@ def show(board):
             elif item == COLOR_BLACK:
                 print('*', end='')
             else:
-                print(item,end='')
+                print(item, end='')
         print('')
     print('')
-if __name__ == '__main__':
-    size = 8
-    ai = AI(8, 1, 5)
-    ai2 = AI(8, -1, 5)
-    def checkflip(board, pos):
-        cnt = 0
-        color = ai.get(board, pos)
-        for dirx in [-1, 0, 1]:
-            for diry in [-1, 0, 1]:
-                if (dirx != 0 or diry != 0):
-                    for step in range(1, size):
-                        second = (pos[0]+dirx*step, pos[1]+diry*step)
-                        if ai.inside(second):
-                            if ai.get(board, second) == COLOR_NONE:
-                                break
-                            elif ai.get(board, second) == color:
-                                if step > 1:
-                                    for t in range(1, step):
-                                        board[pos[0]+dirx *
-                                              t][pos[1]+diry*t] = color
-                                        cnt += 1
-                                else:
-                                    break
-                        else:
-                            break
-        if cnt == 0:
-            raise 'no flip'
-        return board
 
+
+def play(size, ai0: AI, ai1: AI, w0=None, c0=None, w1=None, c1=None):
+    if w0 is not None:
+        ai0.setweights(w0, c0)
+    if w1 is not None:
+        ai1.setweights(w1, c1)
     chessboard = [[0 for _ in range(size)] for _ in range(size)]
     chessboard[3][3] = 1
     chessboard[4][4] = 1
     chessboard[3][4] = -1
     chessboard[4][3] = -1
+    stoptag = 0
     while(True):
-        ai.go(chessboard)
-        if(len(ai.candidate_list) == 0):
-            exit()
-        ai_choice = ai.candidate_list[-1]
+        ai0.go(chessboard)
+        if(len(ai0.candidate_list) == 0):
+            if stoptag == 1:
+                break
+            else:
+                stoptag = 1
+        stoptag = 0
+        ai0_choice = ai0.candidate_list[-1]
 
-        if chessboard[ai_choice[0]][ai_choice[1]] != COLOR_NONE:
+        if chessboard[ai0_choice[0]][ai0_choice[1]] != COLOR_NONE:
             raise "duplicated step"
-        chessboard[ai_choice[0]][ai_choice[1]] = 1
-        chessboard = checkflip(chessboard, ai_choice)
+        chessboard[ai0_choice[0]][ai0_choice[1]] = 1
+        chessboard = ai0.next(chessboard, ai0_choice, ai0.color)
         show(chessboard)
-        ai2.go(chessboard)
-        if(len(ai2.candidate_list) == 0):
-            exit()
-        ai2_choice = ai2.candidate_list[-1]
-        # ai2_choice = (int(input('x')), int(input('y')))
-        if chessboard[ai2_choice[0]][ai2_choice[1]] != COLOR_NONE:
+        ai1.go(chessboard)
+        if(len(ai1.candidate_list) == 0):
+            if stoptag == 1:
+                break
+            else:
+                stoptag = 1
+        stoptag = 0
+        ai1_choice = ai1.candidate_list[-1]
+        if chessboard[ai1_choice[0]][ai1_choice[1]] != COLOR_NONE:
             raise "duplicated step"
-        chessboard[ai2_choice[0]][ai2_choice[1]] = -1
-        chessboard = checkflip(chessboard, ai2_choice)
+        chessboard[ai1_choice[0]][ai1_choice[1]] = -1
+        chessboard = ai1.next(chessboard, ai1_choice, ai1.color)
         show(chessboard)
+    return ai0.count_score(chessboard, ai0.color)
+
+
+if __name__ == '__main__':
+    size = 8
+    ai = AI(8, 1, 5)
+    ai2 = AI(8, -1, 5)
+    play(size, ai, ai2)
